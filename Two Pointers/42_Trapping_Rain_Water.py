06d4d9f453e92fb7584879f09b4c194d96ab13c5
obaid allah ben ali <b.obaid28@gmail.com>
2026-06-24 15:58:21 +0100

    solve problem of Trapping Rain Water :>

---
+++ Two Pointers/42_Trapping_Rain_Water.py
@@ -0,0 +1,14 @@
+def trap(height):
+  n = len(height)
+  max_left = [0] * n
+  max_right = [0] * n
+  max_left[0] = height[0]
+  for i in range(1, n):
+    max_left[i] = max(max_left[i - 1], height[i])
+  max_right[n - 1] = height[n - 1]
+  for i in range(n - 2, -1, -1):
+    max_right[i] = max(max_right[i + 1], height[i])
+  water = 0
+  for i in range(n):
+    water += min(max_left[i],max_right[i]) - height[i]
+  return water
