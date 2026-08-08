ce801751440453c7298a785a3782453d0e980c4c
obaid allah ben ali <b.obaid28@gmail.com>
2026-08-08 03:19:44 +0100

    solve jump

---
+++ Arrays & Hashing/45. Jump Game II.py
@@ -0,0 +1,12 @@
+def jump(nums):
+  jumps = 0
+  current_end = 0
+  max_reach = 0
+
+  for i in range(len(nums) - 1):
+  max_reach = max(max_reach, i + nums[i])
+  if i == current_end:
+  jumps += 1
+  current_end = max_reach
+
+  return jumps
