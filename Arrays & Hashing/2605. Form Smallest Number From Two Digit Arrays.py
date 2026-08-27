92aa82c019c3acfbf77f6da039620f8e2c68ea26
Obaid <b.obaid28@gmail.com>
2026-08-27 05:54:58 +0100

    solve minNumber

---
+++ Arrays & Hashing/2605. Form Smallest Number From Two Digit Arrays.py
@@ -0,0 +1,8 @@
+def minNumber(nums1, nums2):
+    first = min(nums1)
+    second = min(nums2)
+    common =  list(set(nums1) & set(nums2))
+    if(common):
+        return min(common)
+    return   min(int(str(first) + str(second)) , int(str(second) + str(first)))
+    
