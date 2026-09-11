9e7e56ce256f20357617989438d8e917c703d760
Obaid <b.obaid28@gmail.com>
2026-09-11 08:21:26 +0100

    solve totalNumbers

---
+++ Arrays & Hashing/3483. Unique 3-Digit Even Numbers.py
@@ -0,0 +1,17 @@
+def totalNumbers(digits):
+    res = 0
+    useit = set()
+    n = len(digits)
+    for i in range(n):
+        for j in range( n):
+            if( i == j ):
+                continue
+            for k in range(n):
+                if(k == j or k == i):
+                    continue
+                num = int(str(digits[i]) + str(digits[j]) + str(digits[k]))
+                print(num)
+                if(num % 2 ==0 and num not in useit and len(str(num))==3):
+                    res+=1
+                    useit.add(int(str(digits[i]) + str(digits[j]) + str(digits[k])))
+    return res
